5dbcde6fd1c6b1413c25bf56483697582d5814f8
34-anish <luckyanish47@gmail.com>
2022-07-03 16:12:57 +0545

    Modular programming

---
+++ 3_modules/utility/volume.py
@@ -0,0 +1,6 @@
+import math
+def volume_rectangle(length,breadth,height):
+    return length*breadth*height
+
+def volume_cylinder(radius,height):
+    return math.pi*radius**2*height
---
+++ 3_modules/volumecalc.py
@@ -0,0 +1,2 @@
+from utility import volume
+print(volume.volume_rectangle(2,3,4))
