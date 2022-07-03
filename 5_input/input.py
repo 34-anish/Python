73f8ca85c1756dca7dc543f7f4d894a583cc9c19
34-anish <luckyanish47@gmail.com>
2022-07-03 16:40:43 +0545

    Input

---
+++ 5_input/input.py
@@ -0,0 +1,7 @@
+base ,height = input ("Enter base,height(separated with ,): ").split(',')
+print(1/2*int(base)*int(height))
+# 2. Write a program that takes file name with extension as an input and
+# prints just the file name without extension (you can assume that file extensions
+# are always 3 character long)
+program_name , extension = input ("Enter program name: ").split('.')
+print(program_name)
